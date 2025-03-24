f1af9af1b04895ba2578c826f9267dd86883044d
Pooja Bhatti <bhattipooja03@gmail.com>
2025-03-24 11:33:09 +0530

    Overwritting Problmed Solved

--- data_extraction1.py
+++ data_extraction1.py
@@ -2,21 +2,24 @@ import pdfplumber
 import nltk
 import re
 import string
+import os
 
-# Download tokenizer (needed for first-time use)
+
 nltk.download("punkt")
 
 
-def extract_and_tokenize(input_file, output_file="tokens_output.txt"):
+def extract_and_tokenize(input_file):
     ext = input_file.rsplit(".", 1)[-1].lower()
+    base_name = os.path.splitext(input_file)[0]  
+    output_file = f"{base_name}_tokens.txt"  
     text = ""
 
-    # Extract text from TXT
+    
     if ext == "txt":
         with open(input_file, "r", encoding="utf-8") as file:
             text = file.read()
 
-    # Extract text from PDF
+    
     elif ext == "pdf":
         with pdfplumber.open(input_file) as pdf:
             for page in pdf.pages:
@@ -30,16 +33,18 @@ def extract_and_tokenize(input_file, output_file="tokens_output.txt"):
     
     text = re.sub(r"\s+", " ", text)  
     tokens = nltk.word_tokenize(text)  
-
-    
     tokens = [word for word in tokens if word not in string.punctuation]
 
     
     with open(output_file, "w", encoding="utf-8") as out_file:
         out_file.write("\n".join(tokens))  
 
-    print(f"tokens have been saved to {output_file}")
+    print(f"Tokens have been saved to {output_file}")
 
 
-extract_and_tokenize("file.txt")
+
+extract_and_tokenize("file.txt")  
+extract_and_tokenize("file2.pdf")  
+
+
 
